dcb4383b6e8e43ba68af0be11eb3997ae1d97b53
Tianz <ssstz@outlook.com>
2018-08-29 11:26:58 +0800

    fix bug

--- model_cls.py
+++ model_cls.py
@@ -1,24 +1,48 @@
 from keras.layers import Conv1D, MaxPooling1D, Flatten, Dropout, Input, BatchNormalization, Dense
-from keras.layers import Reshape, Lambda
+from keras.layers import Reshape, Lambda, concatenate
 from keras.models import Model
+from keras.engine.topology import Layer
 import numpy as np
 import tensorflow as tf
 
 
-def mat_mul(A, B):
-    return tf.matmul(A, B)
+class MatMul(Layer):
+
+    def __init__(self, **kwargs):
+        super(MatMul, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # Used purely for shape validation.
+        if not isinstance(input_shape, list):
+            raise ValueError('`MatMul` layer should be called '
+                             'on a list of inputs')
+        if len(input_shape) != 2:
+            raise ValueError('The input of `MatMul` layer should be a list containing 2 elements')
+
+        if len(input_shape[0]) != 3 or len(input_shape[1]) != 3:
+            raise ValueError('The dimensions of each element of inputs should be 3')
+
+        if input_shape[0][-1] != input_shape[1][-1]:
+            raise ValueError('The dimension 1 of inputs[0] should match the dimension 0 of inputs[1]')
+
+    def call(self, inputs):
+        if not isinstance(inputs, list):
+            raise ValueError('A `MatMul` layer should be called '
+                             'on a list of inputs.')
+        return tf.matmul(inputs[0], inputs[1])
+
+    def compute_output_shape(self, input_shape):
+        output_shape = [input_shape[0][0], input_shape[0][1], input_shape[1][1]]
+        return tuple(output_shape)
 
 
 def PointNet(nb_classes):
     input_points = Input(shape=(2048, 3))
+    # issues
     # input transformation net
     x = Conv1D(64, 1, activation='relu')(input_points)
     x = BatchNormalization()(x)
     x = Conv1D(128, 1, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv1D(256, 1, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv1D(512, 1, activation='relu')(x)
     x = BatchNormalization()(x)
     x = Conv1D(1024, 1, activation='relu')(x)
     x = BatchNormalization()(x)
@@ -30,11 +54,10 @@ def PointNet(nb_classes):
     x = BatchNormalization()(x)
 
     x = Dense(9, weights=[np.zeros([256, 9]), np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]).astype(np.float32)])(x)
-
     input_T = Reshape((3, 3))(x)
 
     # forward net
-    g = Lambda(mat_mul, arguments={'B': input_T})(input_points)
+    g = MatMul()([input_points, input_T])
     g = Conv1D(64, 1, activation='relu')(g)
     g = BatchNormalization()(g)
     g = Conv1D(64, 1, activation='relu')(g)
@@ -44,10 +67,6 @@ def PointNet(nb_classes):
     f = Conv1D(64, 1, activation='relu')(g)
     f = BatchNormalization()(f)
     f = Conv1D(128, 1, activation='relu')(f)
-    f = BatchNormalization()(f)
-    f = Conv1D(256, 1, activation='relu')(f)
-    f = BatchNormalization()(f)
-    f = Conv1D(512, 1, activation='relu')(f)
     f = BatchNormalization()(f)
     f = Conv1D(1024, 1, activation='relu')(f)
     f = BatchNormalization()(f)
@@ -60,14 +79,10 @@ def PointNet(nb_classes):
     feature_T = Reshape((64, 64))(f)
 
     # forward net
-    g = Lambda(mat_mul, arguments={'B': feature_T})(g)
+    g = MatMul()([g, feature_T])
     g = Conv1D(64, 1, activation='relu')(g)
     g = BatchNormalization()(g)
     g = Conv1D(128, 1, activation='relu')(g)
-    g = BatchNormalization()(g)
-    g = Conv1D(256, 1, activation='relu')(g)
-    g = BatchNormalization()(g)
-    g = Conv1D(512, 1, activation='relu')(g)
     g = BatchNormalization()(g)
     g = Conv1D(1024, 1, activation='relu')(g)
     g = BatchNormalization()(g)
